a7d4fcb39f3f0be0b48552bd09e078af25bea900
Matt Parr <matthew.mgp@gmail.com>
2022-08-04 14:55:25 +1000

    n

--- LPC_install.bat
+++ LPC_install.bat
@@ -4,6 +4,10 @@ git clone https://github.com/vands2013/install_test.git %CD%/LPC
 
 cd LPC
 
-python -m venv seven
+pip install virtualenv
+
+virtualenv seven --python=python3.9.13
+
+:: python -m venv seven
 
 .\seven\Scripts\activate && pip install ipython && ipython install.py
